b773fd969f9b73ef74254dfe2f2fabbdafe4ad9d
Charles Perkins <chuck@kuracali.com>
2017-12-14 13:36:11 -0800

    use BitArray in PitPat

--- src/Wut.jl
+++ src/Wut.jl
@@ -2,17 +2,17 @@ module Wut
 
 struct ScalarEncoder 
 
-	w::Number
-	minval::Number
-	maxval::Number
-	periodic::Bool
-	n::Number
-	radius::Number
-	resolution::Number
-	name::AbstractString
-	verbosity::Number
-	clipInput::Bool
-	forced::Bool
+	w::Number            # width of the contiguous 1 bits, must be odd
+	minval::Number       # minimum value of input signal
+	maxval::Number       # maximum value of input signal (strictly less when periodic = true)
+	periodic::Bool       # input values wrap around and maxval input is mapped onto minval
+	n::Number            # output bit width (must be greater than w)
+	radius::Number       # input value difference > radius do not overlap bits
+	resolution::Number   # input value difference >= resolution do not give identical bits
+	name::AbstractString # optional descriptive string
+	verbosity::Number 
+	clipInput::Bool      # Non-periodic inputs are clipped to minval / maxval
+	forced::Bool         # skip some safety checks
 
 	function ScalarEncoder(;w=0,minval=0,maxval=0,periodic=false,
 				n=0,radius=0,resolution=0,name="",
@@ -24,12 +24,9 @@ end
 export ScalarEncoder
 
 struct BitPat
-	e::Number
-	b::Int64
-	
-	function BitPat(e,b)
-		new(e,b)
-	end
+	e::Unsigned
+	b::BitArray{1}
+	BitPat(e,b) = new(e,BitArray(e))
 end
 export BitPat
 
@@ -40,7 +37,11 @@ end
 export encode
 
 function Base.show(io::IO, m::BitPat)
-       print(io,bin(0,m.e))
+	print(io,"[")
+	for (i,v) in enumerate(m.b)
+		v ? print(io," 1") : print(io," 0")
+	end
+	print(io," ]")
 end
 
 
